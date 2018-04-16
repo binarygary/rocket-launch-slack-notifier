d87db43f5843b3154217349f50268f2181070ce9
Gary Kovar <gkovar@gmail.com>
2018-04-16 08:08:27 -0400

    retruns service provider to its former glory

--- src/Service_Providers/Slack_Provider.php
+++ src/Service_Providers/Slack_Provider.php
@@ -12,7 +12,9 @@ class Slack_Provider implements ServiceProviderInterface {
 	const REDIRECT_URI = 'slack.redirect_uri';
 
 	public function register( Container $container ) {
-
+		$container[ self::REDIRECT_URI ] = function () use ( $container ) {
+			return new Redirect_URI( $container[ Endpoints_Provider::ENDPOINTS_OAUTH ] );
+		};
 	}
 
 }

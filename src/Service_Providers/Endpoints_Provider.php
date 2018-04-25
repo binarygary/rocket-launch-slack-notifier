ad6ab7027998b3354dfaaf90add615fcfe456c9f
Gary Kovar <gkovar@gmail.com>
2018-04-25 11:56:54 -0400

    registers events endpoint

--- src/Service_Providers/Endpoints_Provider.php
+++ src/Service_Providers/Endpoints_Provider.php
@@ -3,21 +3,28 @@
 namespace BinaryGary\Rocket\Service_Providers;
 
 
+use BinaryGary\Rocket\Endpoints\Events;
 use BinaryGary\Rocket\Endpoints\OAuth;
 use Pimple\Container;
 use Pimple\ServiceProviderInterface;
 
 class Endpoints_Provider implements ServiceProviderInterface {
 
-	const ENDPOINTS_OAUTH = 'endpoints.oauth';
+	const ENDPOINTS_OAUTH  = 'endpoints.oauth';
+	const ENDPOINTS_EVENTS = 'endpoints.events';
 
 	public function register( Container $container ) {
 		$container[ self::ENDPOINTS_OAUTH ] = function () use ( $container ) {
 			return new OAuth( $container[ Slack_Provider::POST_MESSAGE ], $container[ Slack_Provider::REDIRECT_URI ] );
 		};
 
+		$container[ self::ENDPOINTS_EVENTS ] = function () {
+			return new Events();
+		};
+
 		add_action( 'rest_api_init', function () use ( $container ) {
 			$container[ self::ENDPOINTS_OAUTH ]->register();
+			$container[ self::ENDPOINTS_EVENTS ]->register();
 		} );
 
 	}

ec2ec3d342a8fbf70dfdcb36289a868e1f433c23
Gary Kovar <gkovar@gmail.com>
2018-04-28 21:23:40 -0400

    injects all locations and providers

---
+++ src/Endpoints/Events/Launch.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace BinaryGary\Rocket\Endpoints\Events;
+
+
+use BinaryGary\Rocket\Launch_Library\Launch as Launch_Message;
+
+class Launch extends Event {
+
+	const KEYWORD = 'launch';
+
+	protected $term;
+	protected $request;
+	protected $request_value;
+
+	public function __construct( Launch_Message $launch, $args ) {
+		$this->term          = $args['term'];
+		$this->request       = $args['request'];
+		$this->request_value = $args['request_value'];
+		parent::__construct( $launch );
+	}
+
+	public function get_keyword() {
+		return self::KEYWORD . ' ' . $this->term;
+	}
+
+	public function query_name() {
+		return $this->request;
+	}
+
+	public function query_value() {
+		return $this->request_value;
+	}
+
+	public function process(): array {
+		foreach ( $this->get_launches() as $launch ) {
+			$this->launch->set( 'title', sprintf( '%s Launch Notice', $this->term ) );
+			$this->launch->set( 'color', 'a71930' );
+			$this->launch->set( 'launch', $launch );
+
+			return $this->launch->message();
+		}
+	}
+
+}
--- src/Service_Providers/Endpoints_Provider.php
+++ src/Service_Providers/Endpoints_Provider.php
@@ -15,7 +15,10 @@ class Endpoints_Provider implements ServiceProviderInterface {
 	const ENDPOINT_EVENTS_COLLECTION = 'endpoints.events.collection';
 	const ENDPOINTS_EVENTS           = 'endpoints.events';
 
-	const ENDPOINT_EVENTS_SPACEX     = 'endpoint.events.spacex';
+	const ENDPOINT_EVENTS_SPACEX = 'endpoint.events.spacex';
+
+	const PROVIDERS_LAUNCHPOINT = 'https://launchlibrary.net/1.4/lsp?limit=100';
+	const LOCATION_LAUNCHPOINT  = 'https://launchlibrary.net/1.4/location?limit=100';
 
 	public function register( Container $container ) {
 		$container[ self::ENDPOINTS_OAUTH ] = function () use ( $container ) {
@@ -26,6 +29,7 @@ class Endpoints_Provider implements ServiceProviderInterface {
 			return new Events\Collection();
 		};
 
+
 		$events = [
 			self::ENDPOINT_EVENTS_SPACEX => Events\SpaceX::class,
 		];
@@ -34,7 +38,7 @@ class Endpoints_Provider implements ServiceProviderInterface {
 			$container[ $event ] = function () use ( $container, $class ) {
 				return new $class( $container[ Launch_Library_Provider::LAUNCH ] );
 			};
-			add_action( 'init', function() use ( $container, $event ) {
+			add_action( 'init', function () use ( $container, $event ) {
 				$container[ self::ENDPOINT_EVENTS_COLLECTION ]->add( $container[ $event ] );
 			} );
 		}
@@ -44,6 +48,32 @@ class Endpoints_Provider implements ServiceProviderInterface {
 		};
 
 		add_action( 'rest_api_init', function () use ( $container ) {
+			$result    = wp_remote_get( self::PROVIDERS_LAUNCHPOINT );
+			$providers = json_decode( $result['body'] );
+			foreach ( $providers->agencies as $agency ) {
+				$container[ sanitize_title( $agency->name ) ] = function () use ( $container, $agency ) {
+					return new Events\Launch( $container[ Launch_Library_Provider::LAUNCH ], [
+						'term'          => $agency->name,
+						'request'       => 'lsp',
+						'request_value' => $agency->id,
+					] );
+				};
+				$container[ self::ENDPOINT_EVENTS_COLLECTION ]->add( sanitize_title( $agency->name ) );
+			}
+
+			$result    = wp_remote_get( self::LOCATION_LAUNCHPOINT );
+			$locations = json_decode( $result['body'] );
+			foreach ( $locations->locations as $location ) {
+				$container[ sanitize_title( $location->name ) ] = function () use ( $container, $location ) {
+					return new Events\Launch( $container[ Launch_Library_Provider::LAUNCH ], [
+						'term'          => $location->name,
+						'request'       => 'locationid',
+						'request_value' => $location->id,
+					] );
+				};
+				$container[ self::ENDPOINT_EVENTS_COLLECTION ]->add( sanitize_title( $location->name ) );
+			}
+
 			$container[ self::ENDPOINTS_OAUTH ]->register();
 			$container[ self::ENDPOINTS_EVENTS ]->register();
 		} );

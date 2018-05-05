00cce63b14ef7be410371ccde2a90d66543728a7
Gary Kovar <gkovar@gmail.com>
2018-05-05 08:26:38 -0400

    fixes output

--- src/Launch_Library/Retriever.php
+++ src/Launch_Library/Retriever.php
@@ -56,7 +56,7 @@ class Retriever {
 		foreach ( $launches->launches as $launch ) {
 			$this->process_launch( $launch );
 			if ( defined( 'WP_CLI' ) && WP_CLI ) {
-				error_log( print_r( json_encode( $this->general_launch_info( $launch ) ), 1 ) );
+				$this->messages->alert( $this->general_launch_info( $launch ) );
 			}
 		}
 
